d6182a72c777490c337b0e1ba23147e229c16eef
Xuan Hao <xuanhao.lim@circles.asia>
2023-01-05 11:08:29 +0800

    added chi router interface

--- http/server/server.go
+++ http/server/server.go
@@ -1,22 +1,25 @@
 package server
 
 import (
+	"errors"
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/cors"
-	"errors"
-	"net/http"
+	"github.com/kelchy/go-lib/log"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
-	"github.com/kelchy/go-lib/log"
 )
 
+// ChiRouter - interface for chi router
+type ChiRouter = chi.Router
 
 // Router - initialized instance
 type Router struct {
-	Engine		*chi.Mux
-	log		log.Log
-	logRequest	bool
+	Engine     *chi.Mux
+	log        log.Log
+	logRequest bool
 }
 
 // New - constructor function to initialize instance
@@ -30,16 +33,16 @@ func New(origins []string) (Router, error) {
 	rtr.logRequest = true
 
 	if len(origins) == 0 {
-		origins = []string{ "http://localhost", "https://localhost" }
+		origins = []string{"http://localhost", "https://localhost"}
 	}
 	rtr.Engine = chi.NewRouter()
 	rtr.Engine.Use(cors.Handler(cors.Options{
-		AllowedOrigins: origins,
-		AllowedMethods: []string{ "GET", "POST", "PUT", "DELETE" },
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders: []string{"Link"},
+		AllowedOrigins:   origins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
-		MaxAge: 12 * 60 * 60,
+		MaxAge:           12 * 60 * 60,
 	}))
 	rtr.Engine.Use(middleware.RealIP)
 	rtr.Engine.Use(rtr.catchall)
@@ -61,7 +64,7 @@ func (rtr *Router) SetLogRequest(lr bool) {
 
 // Run - run and listen for http
 func (rtr Router) Run(proto string, hostport string) error {
-	rtr.log.Out("SERVER_RUN", "Listening " + proto + " " + hostport)
+	rtr.log.Out("SERVER_RUN", "Listening "+proto+" "+hostport)
 	var e error
 	if proto == "http" {
 		e = http.ListenAndServe(hostport, rtr.Engine)
@@ -80,7 +83,7 @@ func (rtr Router) Run(proto string, hostport string) error {
 
 // RunS - run and listen for https
 func (rtr Router) RunS(proto string, hostport string, crt string, key string) error {
-	rtr.log.Out("SERVER_RUNS", "Listening " + proto + " " + hostport)
+	rtr.log.Out("SERVER_RUNS", "Listening "+proto+" "+hostport)
 	var e error
 	if proto == "https" {
 		e = http.ListenAndServeTLS(hostport, crt, key, rtr.Engine)
@@ -98,22 +101,22 @@ func (rtr Router) RunS(proto string, hostport string, crt string, key string) er
 // Static - function to handle and serve static files within a directory on live system
 func (rtr Router) Static(urlPath string, dirPath string) {
 	// do not use wildcard (*) in urlPath
-	rtr.Engine.Handle(urlPath + "*", http.StripPrefix(urlPath, http.FileServer(http.Dir(dirPath))))
+	rtr.Engine.Handle(urlPath+"*", http.StripPrefix(urlPath, http.FileServer(http.Dir(dirPath))))
 	// TODO: handle wildcards better
 }
 
 // StaticFs - function to handle and serve static files embedded into binary using esc
 func (rtr Router) StaticFs(urlPath string, fs http.FileSystem) {
 	/*
-	generate a boxed static filesystem by using esc (https://github.com/mjibson/esc):
-		~/go/bin/esc -o static/static.go -pkg static -ignore=".*.go" ./static
-	on your project's home dir, use it like this:
-		var rtr server.Router
-		rtr.New([]string{})
-		rtr.StaticFs("/static/", static.FS(false))
-	static.FS() returns a standard http.FileSystem which you can pass to this function
+		generate a boxed static filesystem by using esc (https://github.com/mjibson/esc):
+			~/go/bin/esc -o static/static.go -pkg static -ignore=".*.go" ./static
+		on your project's home dir, use it like this:
+			var rtr server.Router
+			rtr.New([]string{})
+			rtr.StaticFs("/static/", static.FS(false))
+		static.FS() returns a standard http.FileSystem which you can pass to this function
 	*/
 	// do not use wildcard (*) in urlPath
-	rtr.Engine.Handle(urlPath + "*", http.FileServer(fs))
+	rtr.Engine.Handle(urlPath+"*", http.FileServer(fs))
 	// TODO: handle wildcards better
 }
